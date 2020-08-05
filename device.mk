c6f10fae494191e8e093940f8452f0171a1ee11c
Marco Rizzo <marco.rizzo.00@outlook.com>
2020-08-05 17:19:52 +0200

    davinci: add Urnyx05 7.3.018 v2.1 GCam heavily based on https://github.com/PixelExperience-Devices/device_xiaomi_grus/commit/9e753c53f3bed5781a576359bbebdc76b44c9d55
    Co-authored-by: Luís Ferreira <contact@lsferreira.net>

--- device.mk
+++ device.mk
@@ -108,7 +108,8 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    libdng_sdk.vendor
+    libdng_sdk.vendor \
+    GoogleCamera
 
 PRODUCT_PACKAGES += \
     libshim_camera
@@ -287,6 +288,10 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/permissions/org.codeaurora.snapcam.xml:system/etc/permissions/org.codeaurora.snapcam.xml
 
 # Power
 PRODUCT_PACKAGES += \
