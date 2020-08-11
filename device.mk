1429ecafbb675388db8a04a5e696c30c4d86b869
kazerine <kamikaze.mrinal@gmail.com>
2020-08-12 00:18:36 +0530

    davinci: Get rid of megvii dependencies in camera.qcom.so
    Change-Id: I97058c320d06819a468ebe38e2704360e9aa91db
    Signed-off-by: Arian <arian.kulmer@web.de>

--- device.mk
+++ device.mk
@@ -111,7 +111,7 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor
 
 PRODUCT_PACKAGES += \
-    libshim_megvii
+    libshim_camera
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
--- extract-files.sh
+++ extract-files.sh
@@ -70,7 +70,7 @@ function blob_fixup() {
     vendor/lib64/hw/camera.qcom.so)
         patchelf --remove-needed "libMegviiFacepp-0.5.2.so" "${2}"
         patchelf --remove-needed "libmegface.so" "${2}"
-        patchelf --add-needed "libshim_megvii.so" "${2}"
+        patchelf --add-needed "libshim_camera.so" "${2}"
         ;;
     esac
 }
