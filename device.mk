ed4102f075db6de1c93dc9c756a0ec7fde61bae4
Twel12 <58646487+Twel12@users.noreply.github.com>
2020-09-28 10:58:49 +0530

    Davinci: use libshim_megvii instead of libshim_camera
    Using libshim_camera causes the camera to crashes the camera on roms like Pixel Experience so using libshim_megvii fixes the issue.

--- device.mk
+++ device.mk
@@ -112,7 +112,7 @@ PRODUCT_PACKAGES += \
     GoogleCamera
 
 PRODUCT_PACKAGES += \
-    libshim_camera
+    libshim_megvii
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
