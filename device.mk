34de8d35bcb6b6ed65ec7243d874f44bf99d9993
Twel12 <shivansh.aeyron@gmail.com>
2020-10-07 21:59:27 +0530

    Include Updater App
    Signed-off-by: Twel12 <shivansh.aeyron@gmail.com>

--- device.mk
+++ device.mk
@@ -291,6 +291,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_davinci/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_davinci/com.nxp.mifare.xml
 
+# OTA
+PRODUCT_PACKAGES += \
+    Updates
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
