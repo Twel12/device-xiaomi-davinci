54c00ae036ce4c2c687bdc60abe1f40bf72ce578
Twel12 <58646487+Twel12@users.noreply.github.com>
2020-12-22 01:03:42 +0530

    davinci: Use Pixel 5 February fingerprint

--- init/init_davinci.cpp
+++ init/init_davinci.cpp
@@ -105,8 +105,7 @@ void vendor_load_properties() {
 
     // SafetyNet workaround
     property_override("ro.boot.verifiedbootstate", "green");
-    fingerprint = "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys";
-    description = "dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys";
+    fingerprint = "google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys";
 
     set_ro_build_prop("fingerprint", fingerprint);
     set_ro_product_prop("device", device);
