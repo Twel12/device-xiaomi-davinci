c3449570cca5922b33c9480bad23f2655b7dada0
daniml3 <danimoral1001@gmail.com>
2021-01-11 17:45:12 +0300

    davinci: include hardware/google/pixel as a soong namespace
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I1b0507998f078aa55ecbaa57ceb668a1b7eca626

--- device.mk
+++ device.mk
@@ -364,7 +364,8 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi
+    hardware/xiaomi \
+    hardware/google/pixel
 
 # Telephony
 PRODUCT_PACKAGES += \
