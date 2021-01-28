0c99a69ac33448549ea1ecb69beef1045ccc47d5
baalajimaestro <me@baalajimaestro.me>
2021-01-28 13:03:29 +0530

    davinci: Optimise dex flags for a faster boot
    * Used multiple threads and speed profile to hasten the first boot
    
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>
    Change-Id: I2cce5ddf7d50308511e81436fcac613b2c6537bf

--- device.mk
+++ device.mk
@@ -116,6 +116,8 @@ PRODUCT_PACKAGES += \
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts
+# Dex
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 
 # Display
 PRODUCT_PACKAGES += \
