dbe7e0d53f347accc134bcede644471b0e50c5e0
Twel12 <shivansh.aeyron@gmail.com>
2021-05-05 22:37:35 +0530

    Update Fingerprint to May

--- init/init_davinci.cpp
+++ init/init_davinci.cpp
@@ -105,7 +105,7 @@ void vendor_load_properties() {
 
     // SafetyNet workaround
     property_override("ro.boot.verifiedbootstate", "green");
-    fingerprint = "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys";
+    fingerprint = "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys";
 
     set_ro_build_prop("fingerprint", fingerprint);
     set_ro_product_prop("device", device);
