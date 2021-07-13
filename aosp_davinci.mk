d5e6c04f7f8dd1dfd2b264e9c47085d0ac5ef265
Twel12 <shivansh.aeyron@gmail.com>
2021-07-13 16:38:38 +0530

    aospify

--- aosp_davinci.mk
+++ aosp_davinci.mk
@@ -10,26 +10,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 
-# Inherit some common Xtended stuff.
+# Inherit some common aosp stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_INCLUDE_WIFI_EXT := true
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := davinci
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := xtended_davinci
+PRODUCT_NAME := aosp_davinci
 PRODUCT_MODEL := Mi 9T
 
-# Official MSM-Xtended
-XTENDED_BUILD_TYPE := OFFICIAL
-
-# Maintainer
-XTENDED_BUILD_MAINTAINER := SVB22
 
 # FOD animations
 EXTRA_FOD_ANIMATIONS := true
